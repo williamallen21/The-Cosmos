0cf46e8cae1912651d9c2d239dff9609bea33ec4
williamallen21 <45079168+williamallen21@users.noreply.github.com>
2018-11-17 00:15:59 -0800

    Update Cosmos.java

--- Cosmos.java
+++ Cosmos.java
@@ -23,10 +23,23 @@ public class Main
 
 String decision1 = sc.nextLine();
 if (decision1.equals("search")) {
-  System.out.println("You have found a lab"); 
+  System.out.println("You have found a lab \n Do you want to (fix) (search) "); 
 } else if (decision1.equals("leave")) {
   System.out.println("Can not leave Venus until propulsion system is fixed");
 }
 
+String decision2 = sc.nextLine();
+if (decision2.equals("fix")) {
+  System.out.println("Propulsion system repaired \n Do you want to (move) (leave)"); 
+} else if (decision2.equals("search")) {
+  System.out.println("Nothing new to discover");
+
+/*  String decision3 = sc.nextLine();
+if (decision3.equals("move")) {
+  System.out.println("Propulsion system repaired \n Do you want to (move) (leave)"); 
+} else if (decision3.equals("leave")) {
+  System.out.println("Nothing new to discover");
 }
-      }
+*/
+}
+  
